a882c09ac082b170284a7b89b9706e5bbe3c4a76
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-02-17 22:00:41 +0100

    Initial commit

---
+++ src/AuthorizationServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace QRFeedz\Auhtorization;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthorizationServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
