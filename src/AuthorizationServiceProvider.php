1f3cfe59732e603c312a2fed9deee63edd69dfac
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-02-17 22:57:44 +0100

    wip

--- src/AuthorizationServiceProvider.php
+++ src/AuthorizationServiceProvider.php
@@ -1,14 +1,17 @@
 <?php
 
-namespace QRFeedz\Auhtorization;
+namespace QRFeedz\Authorization;
 
 use Illuminate\Support\ServiceProvider;
+use QRFeedz\Authorization\Gates\CountryGates;
+use QRFeedz\Authorization\Gates\UserGates;
 
 class AuthorizationServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        //
+        CountryGates::apply();
+        UserGates::apply();
     }
 
     public function register()
---
+++ src/Gates/UserGates.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace QRFeedz\Authorization\Gates;
+
+use Illuminate\Support\Facades\Gate;
+
+class UserGates
+{
+    public static function apply()
+    {
+        Gate::define('update-post', function (User $user, Post $post) {
+            return $user->id === $post->user_id;
+        });
+    }
+}
