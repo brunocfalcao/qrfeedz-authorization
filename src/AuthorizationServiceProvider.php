d8846d013a3fe50c6289b7c60d5d821e310b8455
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-02-26 17:53:47 +0100

    wip

--- src/AuthorizationServiceProvider.php
+++ src/AuthorizationServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace QRFeedz\Authorization;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use QRFeedz\Authorization\Gates\CategoryGates;
 use QRFeedz\Authorization\Gates\CountryGates;
@@ -13,10 +14,40 @@ use QRFeedz\Authorization\Gates\ResponseGates;
 use QRFeedz\Authorization\Gates\TagGates;
 use QRFeedz\Authorization\Gates\UserGates;
 use QRFeedz\Authorization\Gates\WidgetGates;
+use QRFeedz\Authorization\Policies\CategoryPolicy;
+use QRFeedz\Authorization\Policies\CountryPolicy;
+use QRFeedz\Authorization\Policies\OrganizationPolicy;
+use QRFeedz\Authorization\Policies\PlacePolicy;
+use QRFeedz\Authorization\Policies\QuestionnairePolicy;
+use QRFeedz\Authorization\Policies\QuestionPolicy;
+use QRFeedz\Authorization\Policies\ResponsePolicy;
+use QRFeedz\Authorization\Policies\TagPolicy;
+use QRFeedz\Authorization\Policies\UserPolicy;
+use QRFeedz\Authorization\Policies\WidgetPolicy;
+use QRFeedz\Cube\Models\Category;
+use QRFeedz\Cube\Models\Country;
+use QRFeedz\Cube\Models\Organization;
+use QRFeedz\Cube\Models\Place;
+use QRFeedz\Cube\Models\Question;
+use QRFeedz\Cube\Models\Questionnaire;
+use QRFeedz\Cube\Models\Tag;
+use QRFeedz\Cube\Models\User;
+use QRFeedz\Cube\Models\Widget;
 
 class AuthorizationServiceProvider extends ServiceProvider
 {
     public function boot()
+    {
+        $this->registerGates();
+        $this->registerPolicies();
+    }
+
+    public function register()
+    {
+        //
+    }
+
+    protected function registerGates()
     {
         TagGates::apply();
         UserGates::apply();
@@ -30,8 +61,17 @@ class AuthorizationServiceProvider extends ServiceProvider
         QuestionnaireGates::apply();
     }
 
-    public function register()
+    protected function registerPolicies(): void
     {
-        //
+        Gate::policy(Tag::class, TagPolicy::class);
+        Gate::policy(User::class, UserPolicy::class);
+        Gate::policy(Place::class, PlacePolicy::class);
+        Gate::policy(Widget::class, WidgetPolicy::class);
+        Gate::policy(Country::class, CountryPolicy::class);
+        Gate::policy(Question::class, QuestionPolicy::class);
+        Gate::policy(Category::class, CategoryPolicy::class);
+        Gate::policy(Response::class, ResponsePolicy::class);
+        Gate::policy(Organization::class, OrganizationPolicy::class);
+        Gate::policy(Questionnaire::class, QuestionnairePolicy::class);
     }
 }
