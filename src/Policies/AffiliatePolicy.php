7de55c354e23d2e455cb8084690cac17a0576e8f
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-03-17 09:57:02 +0100

    Update AffiliatePolicy.php

--- src/Policies/AffiliatePolicy.php
+++ src/Policies/AffiliatePolicy.php
@@ -2,7 +2,7 @@
 
 namespace QRFeedz\Authorization\Policies;
 
-use QRFeedz\Cube\Models\Authorization;
+use QRFeedz\Cube\Models\Affiliate;
 use QRFeedz\Cube\Models\User;
 
 class AffiliatePolicy
@@ -18,7 +18,7 @@ class AffiliatePolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Authorization $model): bool
+    public function view(User $user, Affiliate $model): bool
     {
         return true;
     }
@@ -34,7 +34,7 @@ class AffiliatePolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Authorization $model): bool
+    public function update(User $user, Affiliate $model): bool
     {
         return true;
     }
@@ -42,7 +42,7 @@ class AffiliatePolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Authorization $model): bool
+    public function delete(User $user, Affiliate $model): bool
     {
         return true;
     }
@@ -50,7 +50,7 @@ class AffiliatePolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Authorization $model): bool
+    public function restore(User $user, Affiliate $model): bool
     {
         return true;
     }
@@ -58,7 +58,7 @@ class AffiliatePolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Authorization $model): bool
+    public function forceDelete(User $user, Affiliate $model): bool
     {
         return true;
     }
