c67d370e9d6b216f1dcf7417cad4deef01afdb7f
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-03-26 20:44:12 +0200

    wip

---
+++ src/Policies/PageTypePolicy.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace QRFeedz\Authorization\Policies;
+
+use QRFeedz\Cube\Models\PageType;
+use QRFeedz\Cube\Models\User;
+
+class PageTypePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, PageType $model): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, PageType $model): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, PageType $model): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, PageType $model): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, PageType $model): bool
+    {
+        return true;
+    }
+}
