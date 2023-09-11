abe9c029d3f2c1a4605b024eee564efa27de10b7
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-09-12 00:07:52 +0200

    Update OpenAIPromptPolicy.php

--- src/Policies/OpenAIPromptPolicy.php
+++ src/Policies/OpenAIPromptPolicy.php
@@ -9,36 +9,50 @@ class OpenAIPromptPolicy
 {
     public function viewAny(User $user)
     {
-        return true;
+        return $user->isSuperAdmin();
     }
 
     public function view(User $user, OpenAIPrompt $model)
     {
-        return true;
+        return $user->isSuperAdmin();
     }
 
     public function create(User $user)
     {
-        return true;
+        return
+            // User is super admin.
+            $user->isSuperAdmin();
     }
 
     public function update(User $user, OpenAIPrompt $model)
     {
-        return true;
+        return
+            // User is super admin.
+            $user->isSuperAdmin() ||
+
+            // User is an affiliate.
+            $user->isAffiliate();
     }
 
     public function delete(User $user, OpenAIPrompt $model)
     {
-        return true;
+        // There should always be an OpenAI prompt, associated.
+        return false;
     }
 
     public function restore(User $user, OpenAIPrompt $model)
     {
-        return true;
+        return false;
     }
 
     public function forceDelete(User $user, OpenAIPrompt $model)
     {
-        return true;
+        return false;
+    }
+
+    public function replicate(User $user, OpenAiPrompt $model)
+    {
+        // Replication is disabled.
+        return false;
     }
 }
