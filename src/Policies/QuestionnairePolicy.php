35d11393ab4462af343ff71ee6d98b85f55fa2e1
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-09-13 00:05:03 +0200

    Update QuestionnairePolicy.php

--- src/Policies/QuestionnairePolicy.php
+++ src/Policies/QuestionnairePolicy.php
@@ -14,31 +14,73 @@ class QuestionnairePolicy
 
     public function view(User $user, Questionnaire $model)
     {
-        return true;
+        /**
+         * Super admin? All good.
+         */
+        if ($user->isSuperAdmin()) {
+            return true;
+        }
+
+        /**
+         * Return questionnaires that are part of this user as client admin.
+         */
+        if ($user->isAtLeastAuthorizedAs('client-admin')) {
+            return $model->client_id == $user->client_id;
+        }
+
+        return false;
     }
 
     public function create(User $user)
     {
-        return true;
+        return
+            // User is super admin.
+            $user->isSuperAdmin() ||
+
+            // User is affiliate.
+            $user->isAffiliate() ||
+
+            // User is client-admin.
+            $user->isAtLeastAuthorizedAs('client-admin');
     }
 
     public function update(User $user, Questionnaire $model)
     {
-        return true;
+        /**
+         * Super admin? All good.
+         */
+        if ($user->isSuperAdmin()) {
+            return true;
+        }
+
+        /**
+         * Return questionnaires that are part of this user as client admin.
+         */
+        if ($user->isAtLeastAuthorizedAs('client-admin')) {
+            return $model->client_id == $user->client_id;
+        }
+
+        return false;
     }
 
     public function delete(User $user, Questionnaire $model)
     {
-        return true;
+        return $user->isSuperAdmin();
     }
 
     public function restore(User $user, Questionnaire $model)
     {
-        return true;
+        return false;
     }
 
     public function forceDelete(User $user, Questionnaire $model)
     {
-        return true;
+        return false;
+    }
+
+    public function replicate(User $user, Questionnaire $model)
+    {
+        // Replication is disabled.
+        return false;
     }
 }
