4e6bb736b6826ddb86bf1347b358f891a2892c58
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-09-12 16:55:19 +0200

    Update PageInstancePolicy.php

--- src/Policies/PageInstancePolicy.php
+++ src/Policies/PageInstancePolicy.php
@@ -9,36 +9,41 @@ class PageInstancePolicy
 {
     public function viewAny(User $user)
     {
-        return true;
+        return $user->isSuperAdmin();
     }
 
     public function view(User $user, PageInstance $model)
     {
-        return true;
+        return $user->isAllowedAdminAccess();
     }
 
     public function create(User $user)
     {
-        return true;
+        return $user->isSuperAdmin();
     }
 
     public function update(User $user, PageInstance $model)
     {
-        return true;
+        return $user->isSuperAdmin();
     }
 
     public function delete(User $user, PageInstance $model)
     {
-        return true;
+        return $user->isSuperAdmin();
     }
 
     public function restore(User $user, PageInstance $model)
     {
-        return true;
+        return false;
     }
 
     public function forceDelete(User $user, PageInstance $model)
     {
-        return true;
+        return false;
+    }
+
+    public function replicate(User $user, PageInstance $model)
+    {
+        return false;
     }
 }
