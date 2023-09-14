c3fe08324d842b8a02828cf224882d3f829c1bdf
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-09-14 20:44:11 +0200

    wip

--- src/Policies/QuestionInstancePolicy.php
+++ src/Policies/QuestionInstancePolicy.php
@@ -9,36 +9,63 @@ class QuestionInstancePolicy
 {
     public function viewAny(User $user)
     {
-        return true;
+        return $user->isAllowedAdminAccess();
     }
 
     public function view(User $user, QuestionInstance $model)
     {
-        return true;
+        return $user->isAllowedAdminAccess();
     }
 
     public function create(User $user)
     {
-        return true;
+        return
+            // User is super admin.
+            $user->isSuperAdmin() ||
+
+            // User is an affiliate.
+            $user->isAffiliate();
     }
 
     public function update(User $user, QuestionInstance $model)
     {
-        return true;
+        return
+            // User is super admin.
+            $user->isSuperAdmin() ||
+
+            // User is an affiliate.
+            $user->isAffiliate();
     }
 
     public function delete(User $user, QuestionInstance $model)
     {
-        return true;
+        return
+            // User is super admin.
+            $user->isSuperAdmin() ||
+
+            // User is an affiliate.
+            $user->isAffiliate();
     }
 
     public function restore(User $user, QuestionInstance $model)
     {
-        return true;
+        return false;
     }
 
     public function forceDelete(User $user, QuestionInstance $model)
     {
-        return true;
+        return false;
+    }
+
+    public function replicate(User $user, QuestionInstance $model)
+    {
+        // Replication is disabled.
+        return false;
+    }
+
+    public function addResponse(User $user, QuestionInstance $model)
+    {
+        // Responses cannot be created in Question instances.
+        return false;
     }
 }
